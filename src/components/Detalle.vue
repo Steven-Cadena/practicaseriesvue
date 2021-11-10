3b76677900729d394ea087953194d9e62feabead
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-11-10 14:18:50 +0100

    commit

---
+++ src/components/Detalle.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Detalle series</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Detalle",
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+import Home from './components/Home.vue';
+// import Detalle from './components/Detalle.vue';
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  {path: "/", components: Home},
+  // {path: "/home", components: Home},
+  // {path: "/detalle/:idserie", components: Detalle},
+]
+
+const router = new VueRouter({
+  routes,
+  mode:"history"
+});
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
